ebba73c87d517d80ce6da3308b34afe83bdf4bca
Simone Picciani <zanza00@gmail.com>
2018-03-20 00:11:05 +0100

    dynamic home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
-import Pokemon from './components/Pokemon';
+import Pokemon from "./components/Pokemon";
 
-import logo from './Poke_Ball_icon.svg';
-import './App.css';
+import logo from "./Poke_Ball_icon.svg";
+import "./App.css";
+
+const pokemonList = [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        types: ["grass"]
+    },
+    {
+        id: 4,
+        name: "Charizard",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+        types: ["fire"]
+    },
+    {
+        id: 7,
+        name: "Squirtle",
+        image:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+        types: ["water"]
+    }
+];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 class App extends PureComponent {
     constructor() {
         super();
 
         this.state = {
-            pokemon: {
-                name: 'Bulbasaur',
-                image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-                types: ['grass']
-
-            },
+            pokemon: pokemonList[getRandomInt(pokemonList.length)]
         };
 
         this.handlePokemonChange = this.handlePokemonChange.bind(this);
@@ -23,23 +46,28 @@ class App extends PureComponent {
 
     handlePokemonChange(e) {
         console.log(e.target.value);
+        const selectedPokemon = pokemonList.find(
+            ({ id }) => id === parseInt(e.target.value, 10)
+        );
+        this.setState({ pokemon: selectedPokemon });
     }
+
     render() {
-        const {pokemon} = this.state;
+        const { pokemon } = this.state;
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Choose your Pokemon</h1>
                 </header>
-                <select onChange={this.handlePokemonChange}>
-                    <option value="1">Bulbasaur</option>
-                    <option value="4">Charizard</option>
-                    <option value="7">Squirtle</option>
+                <select onChange={this.handlePokemonChange} value={pokemon.id}>
+                    {pokemonList.map(pokemon => (
+                        <option key={pokemon.id} value={pokemon.id}>
+                            {pokemon.name}
+                        </option>
+                    ))}
                 </select>
-                <Pokemon
-                    {...pokemon}
-                />
+                <Pokemon {...pokemon} />
             </div>
         );
     }
