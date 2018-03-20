a97c72207873eaef513d12d574dfca7033eeb3ea
Simone Picciani <zanza00@gmail.com>
2018-03-21 00:07:15 +0100

    Error in bulbasaur testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App, { parseResponse } from "./App";
 import squirtle from "./mock/squirtle";
+import bulbasaur from "./mock/bulbasaur";
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
@@ -21,6 +22,22 @@ describe("Response Parse", () => {
             image:
                 "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
             types: ["water"]
+        };
+
+        const result = parseResponse(response);
+
+        expect(result).toEqual(expected);
+    });
+    it("can parse the response for bulbbasaur", () => {
+        const response = bulbasaur;
+
+        // Same object that we had before
+        const expected = {
+            id: 1,
+            name: "Bulbasaur",
+            image:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+            types: ["grass", "poison"]
         };
 
         const result = parseResponse(response);
