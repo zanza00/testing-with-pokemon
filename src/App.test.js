5254af4bccbd4a2a06be6ef8cf208fec777c0da0
Simone Picciani <zanza00@gmail.com>
2018-04-16 22:22:35 +0200

    add resources to to be tested

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import squirtle from './response/squirtle'
+import charmender from './response/charmender'
+import bulbasaur from './response/bulbasaur'
+
 import App from './App';
 
 it('renders without crashing', () => {
